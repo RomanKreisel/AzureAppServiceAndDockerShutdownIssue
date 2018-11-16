d7a6c83850cc5cabf999a4c19b654058fd2577fd
Roman Kreisel <roman.kreisel@festo.com>
2018-11-16 14:14:29 +0100

    Add project files.

---
+++ AzureAppServiceAndDockerShutdownIssue/Startup.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace AzureAppServiceAndDockerShutdownIssue
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(
+            IApplicationBuilder app,
+            IHostingEnvironment env,
+            IApplicationLifetime applicationLifetime
+        )
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            applicationLifetime.ApplicationStarted.Register(() => Console.WriteLine("Application started at " + DateTime.Now));
+            applicationLifetime.ApplicationStopping.Register(() => Console.WriteLine("Application stopping at " + DateTime.Now));
+            applicationLifetime.ApplicationStopped.Register(() => Console.WriteLine("Application stopped at " + DateTime.Now));
+
+            app.UseMvc();
+        }
+    }
+}
